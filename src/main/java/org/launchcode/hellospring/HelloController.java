a95785458c5cc415fa1bf8c47bc62aa55b688b13
Teresa <teresatull@comcast.net>
2022-10-21 02:44:38 -0500

    ch 13 java static and dynamic templates

--- src/main/java/org/launchcode/hellospring/HelloController.java
+++ src/main/java/org/launchcode/hellospring/HelloController.java
@@ -3,54 +3,109 @@
 
 package org.launchcode.hellospring;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Chris Bay
  */
 @Controller
 @RequestMapping("hello")
+//lives at /hello
 public class HelloController {
 
-    //    // Handle request at path /hello
+     // Handle request at path /hello (ch 10)
    //public class HelloController {
 //        return "Hello, Spring!";
 //    }
 
-        // lives at /hello/goodbye
+        // lives at /hello/goodbye (ch 10)
         @GetMapping("goodbye")
         @ResponseBody
         public String goodbye() {
             return "Goodbye, Spring!";
         }
 
-        //    // Handles requests of the form /hello?name=LaunchCode
+        //    // Handles requests of the form /hello?name=LaunchCode (ch 10)
 //    @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST})
 //    public String helloWithQueryParam(@RequestParam String name) {
 //        return "Hello, " + name + "!";
 //    }
-        // Handles requests of the form /hello?name=LaunchCode
-        @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST})
-        @ResponseBody
-        public String helloWithQueryParam(@RequestParam String name) {
-            return "Hello, " + name + "!";
-        }
+        // Handles requests of the form /hello?name=LaunchCode (ch 10)
+//        @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST})
+//        @ResponseBody
+//        public String helloWithQueryParam(@RequestParam String name) {
+//            return "Hello, " + name + "!";
+//        }
 
-        // Handles requests of the form /hello/LaunchCode
-        @GetMapping("{name}")
-        @ResponseBody
-        public String helloWithPathParam(@PathVariable String name) {
-            return "Hello, " + name + "!";
-        }
+        // Handles requests of the form /hello/LaunchCode (ch 10)
+//        @GetMapping("{name}")
+//        @ResponseBody
+//        public String helloWithPathParam(@PathVariable String name) {
+//            return "Hello, " + name + "!";
+//        }
 
-        // /hello/form
+//    // /hello/form (ch 10)
+//    @GetMapping("form")
+//    public String helloForm() {
+//        return "<html>" +
+//                "<body>" +
+//                "<form action = '/hello/hello' method = 'post'>" +
+//                "<input type = 'text' name = 'name' />" +
+//                "<select name = 'language' id='lang'>" +
+//                "<option value = 'English'>English</option>" +
+//                "<option value = 'French'>French</option>" +
+//                "<option value = 'Spanish'>Spanish</option>" +
+//                "<option value = 'German'>German</option>" +
+//                "<option value = 'Italian'>Italian</option>" +
+//                "<option value = 'Japanese'>Japanese</option>" +
+//                "</select>" +
+//                "<input type = 'submit' value = 'Greet Me!' >" +
+//                "</form>" +
+//                "</body>" +
+//                "</html>";
+//    }
+
+    // /hello/form using Thymeleaf form
+    // ch 13
         @GetMapping("form")
         public String helloForm() {
             return "form";
         }
+    // Handles requests of the form /hello?name=LaunchCode using Thymeleaf hello
+    // ch 13
+    @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST})
+    public String helloWithThymeleaf(@RequestParam String name, Model model) {
+        String theGreeting = "Hello, " + name + "!";
+        model.addAttribute("greeting", theGreeting);
+            return "hello";
+    }
 
-        //    Exercises
+    // Handles requests of the form /hello/LaunchCode using Thymeleaf hello
+    // ch 13
+    // can do like code above or not use variable theGreeting as below
+    @GetMapping("{name}")
+    public String helloWithPathParam(@PathVariable String name, Model model) {
+        model.addAttribute("greeting", "Hello, " + name + "!");
+        return "hello";
+    }
 
+    // Handles requests of the form /hello-names using Thymeleaf hello
+    // ch 13
+    @GetMapping("hello-names")
+    public String helloNames(Model model){
+        List<String> names = new ArrayList<>();
+        names.add("LaunchCode");
+        names.add("Java");
+        names.add("JavaScript");
+        model.addAttribute("names", names);
+        return "hello-list";
+    }
 
+    //    Exercises (ch 10)
         @RequestMapping(method = RequestMethod.POST, value = "hello")
         @ResponseBody
         public String helloPost(@RequestParam String name, @RequestParam String language) {
